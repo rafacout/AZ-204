d81aaff8d585cdffaacce3b81cf014836286a1c6
Rafael Coutinho <coutinhor@knowfully.com>
2022-02-14 21:17:11 -0300

    created a function app

--- WebApp/WebApp1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApp/WebApp1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("72506b83-58a2-4852-9bfd-9843affb5cfd")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
